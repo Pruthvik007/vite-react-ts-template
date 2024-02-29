6f3caf62bcaa7368bcf4e55781ae524c662428fd
Sai Pruthvik <saipruthvik07@gmail.com>
2024-02-29 22:47:40 +0530

    nav-bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,79 +1,75 @@
 import { useState } from "react";
+import { BASE_URL } from "../constants/Config";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./common/ThemeToggle";
+import CrossIcon from "../assets/svg-icons/CrossIcon";
+import MenuIcon from "../assets/svg-icons/MenuIcon";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Define navigation items
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleNav = () => {
+    setIsVisible(!isVisible);
+  };
   const navItems = [
-    { name: "Home", path: "/" },
-    { name: "About", path: "/about" },
-    { name: "Contact", path: "/contact" },
+    { label: "Home", to: BASE_URL },
+    { label: "About", to: `${BASE_URL}/about` },
+    { label: "Contact", to: `${BASE_URL}/contact` },
   ];
 
   return (
-    <nav className="secondary text-white min-w-full py-1">
-      <div className="flex justify-between items-center px-3">
-        <div className="flex items-center gap-3 md:gap-8">
-          <div>
-            <Link to="/" className="flex items-center">
-              Logo
-            </Link>
-          </div>
-          <div id="primary-nav" className="hidden md:flex items-center">
-            {/* Adjusted spacing between links */}
-            {navItems.map((item) => (
-              <Link
-                key={item.name}
-                to={item.path}
-                className="p-2 hover:text-gray-400"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div className="flex gap-3">
-          <ThemeToggle />
-          <div id="mobile-menu-button" className="md:hidden flex items-center">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="mobile-menu-button"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        id="mobile-nav"
-        className={`${isOpen ? "block" : "hidden"} md:hidden px-1`}
-      >
+    <div className="bg-black flex justify-between items-center h-20 px-4 text-white">
+      <Link id="desktop-logo" to={BASE_URL}>
+        <h1 className="w-full text-xl md:text-3xl font-bold text-[#00df9a]">
+          React + TypeScript
+        </h1>
+      </Link>
+      <ul id="desktop-nav-items" className="hidden md:flex md:items-center">
         {navItems.map((item) => (
-          <Link
-            key={item.name}
-            to={item.path}
-            className="block p-2 text-sm hover:bg-gray-700"
+          <li
+            key={item.label}
+            className="p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
           >
-            {item.name}
-          </Link>
+            <Link to={item.to}>{item.label}</Link>
+          </li>
         ))}
+        <li className="p-2 rounded-xl m-2 cursor-pointer hover:bg-[#00df9a] flex items-center">
+          <ThemeToggle className="w-10 h-10" />
+        </li>
+      </ul>
+      <div
+        id="mobile-nav-toggle"
+        onClick={toggleNav}
+        className="block md:hidden"
+      >
+        {isVisible ? <CrossIcon /> : <MenuIcon />}
       </div>
-    </nav>
+      <ul
+        id="mobile-nav-items"
+        className={
+          isVisible
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+        }
+      >
+        <div className="flex items-center justify-around p-3 gap-x-3">
+          <Link id="mobile-logo" to={BASE_URL}>
+            <h1 className="w-full font-bold text-[#00df9a] text-xl">
+              React + TypeScript
+            </h1>
+          </Link>
+          <ThemeToggle className="w-8 h-8" />
+        </div>
+        {navItems.map((item) => (
+          <li
+            onClick={toggleNav}
+            key={item.to}
+            className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            <Link to={item.to}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        // light: {
-        //   primary: "#3C4042", // Dark grey
-        //   secondary: "#5F6368", // Grey
-        //   success: "#34A853", // Green
-        //   error: "#EA4335", // Red
-        //   warning: "#FBBC05", // Yellow
-        //   info: "#4285F4", // Blue
-        // },
-        // dark: {
-        //   primary: "#222831", // Charcoal
-        //   secondary: "#393E46", // Iron
-        //   success: "#006400", // Dark Green
-        //   error: "#D00000", // Lusty Red
-        //   warning: "#FFBA08", // Selective Yellow
-        //   info: "#0081A7", // Cerulean Blue
-        // },
       },
     },
   },
